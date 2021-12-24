26c3c27920f73652a0e5b52ad6f3afa3f28c30f0
kjg.leh <kjg.leh@gmail.com>
2021-12-24 23:21:06 +0900

    set eureka server

--- src/main/kotlin/me/kjgleh/msadiscovery/MsaDiscoveryApplication.kt
+++ src/main/kotlin/me/kjgleh/msadiscovery/MsaDiscoveryApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class MsaDiscoveryApplication
 
 fun main(args: Array<String>) {
